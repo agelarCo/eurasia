c7b252a1ff14191006cc39179a4df821ecf0a08b
Grigory-F <grishka2500@gmail.com>
2023-03-10 15:54:07 +0400

    fix tasks

--- gulpfile.js
+++ gulpfile.js
@@ -36,10 +36,10 @@ function watcher() {
 // Последовательная обработка шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle)
 // Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images))
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, svgSprive, scss, js, images))
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
-const build = gulp.series(reset, mainTasks)
+const build = gulp.series(reset,  mainTasks)
 const deployZIP = gulp.series(reset, mainTasks, zip)
 const deployFTP = gulp.series(reset, mainTasks, ftp)
 
