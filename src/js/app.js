cd58f871b3435d8eaf313bc89b60a57f966ba75c
Grigory-F <grishka2500@gmail.com>
2023-03-17 16:04:22 +0400

    awdawdwad

--- src/js/app.js
+++ src/js/app.js
@@ -45,99 +45,103 @@ if (
   });
 }
 
-/* 
 function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
   const [from, to] = getParsed(fromInput, toInput);
-  fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+  fillSlider(fromInput, toInput, "transparent", "#25daa5", controlSlider);
   if (from > to) {
-      fromSlider.value = to;
-      fromInput.value = to;
+    fromSlider.value = to;
+    fromInput.value = to;
   } else {
-      fromSlider.value = from;
+    fromSlider.value = from;
   }
 }
-  
+
 function controlToInput(toSlider, fromInput, toInput, controlSlider) {
   const [from, to] = getParsed(fromInput, toInput);
-  fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+  fillSlider(fromInput, toInput, "transparent", "#ff3048", controlSlider);
   setToggleAccessible(toInput);
   if (from <= to) {
-      toSlider.value = to;
-      toInput.value = to;
+    toSlider.value = to;
+    toInput.value = to;
   } else {
-      toInput.value = from;
+    toInput.value = from;
   }
 }
 
 function controlFromSlider(fromSlider, toSlider, fromInput) {
-const [from, to] = getParsed(fromSlider, toSlider);
-fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
-if (from > to) {
-  fromSlider.value = to;
-  fromInput.value = to;
-} else {
-  fromInput.value = from;
-}
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "transparent", "#ff3048", toSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromInput.value = from;
+  }
 }
 
 function controlToSlider(fromSlider, toSlider, toInput) {
-const [from, to] = getParsed(fromSlider, toSlider);
-fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
-setToggleAccessible(toSlider);
-if (from <= to) {
-  toSlider.value = to;
-  toInput.value = to;
-} else {
-  toInput.value = from;
-  toSlider.value = from;
-}
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "transparent", "#ff3048", toSlider);
+  setToggleAccessible(toSlider);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
+    toSlider.value = from;
+  }
 }
 
 function getParsed(currentFrom, currentTo) {
-const from = parseInt(currentFrom.value, 10);
-const to = parseInt(currentTo.value, 10);
-return [from, to];
+  const from = parseInt(currentFrom.value, 10);
+  const to = parseInt(currentTo.value, 10);
+  return [from, to];
 }
 
 function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-  const rangeDistance = to.max-to.min;
+  const rangeDistance = to.max - to.min;
   const fromPosition = from.value - to.min;
   const toPosition = to.value - to.min;
   controlSlider.style.background = `linear-gradient(
     to right,
     ${sliderColor} 0%,
-    ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
-    ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
-    ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
-    ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
+    ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+    ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+    ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
+    ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
     ${sliderColor} 100%)`;
 }
 
 function setToggleAccessible(currentTarget) {
-const toSlider = document.querySelector('#toSlider');
-if (Number(currentTarget.value) <= 0 ) {
-  toSlider.style.zIndex = 2;
-} else {
-  toSlider.style.zIndex = 0;
-}
+  const toSlider = document.querySelector(
+    ".filter-btn-dropdown-range__input-range--from"
+  );
+  if (Number(currentTarget.value) <= 0) {
+    toSlider.style.zIndex = 2;
+  } else {
+    toSlider.style.zIndex = 0;
+  }
 }
 
-const fromSlider = document.querySelector('.filter-btn-dropdown-range__input-range--from');
-const toSlider = document.querySelector('.filter-btn-dropdown-range__input-range--to');
-const fromInput = document.querySelector('#fromInput');
-const toInput = document.querySelector('#toInput');
-fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+const fromSlider = document.querySelector(
+  ".filter-btn-dropdown-range__input-range--from"
+);
+const toSlider = document.querySelector(
+  ".filter-btn-dropdown-range__input-range--to"
+);
+const fromInput = document.querySelector(
+  ".filter-btn-dropdown-range__input--from"
+);
+const toInput = document.querySelector(".filter-btn-dropdown-range__input--to");
+fillSlider(fromSlider, toSlider, "transparent", "#ff3048", toSlider);
 setToggleAccessible(toSlider);
 
 fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
 toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
-fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+fromInput.oninput = () =>
+  controlFromInput(fromSlider, fromInput, toInput, toSlider);
 toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
 
- */
-
-/* filter-line-btn filter-line-btn--col */
-/* products-grid */
 const btnColExpand = document.querySelector(".filter-line-btn--col");
 const btnRowExpand = document.querySelector(".filter-line-btn--row");
 const productsGrid = document.querySelector(".products-grid");
@@ -281,6 +285,15 @@ headerMenuDropdownListsLinks.forEach((element) => {
 document.querySelectorAll(".products-card-char-list").forEach((elem, index) => {
   new SimpleBar(elem);
 });
+
+/* let boxForfilterCheckboxes = document.querySelectorAll('.filter-btn-dropdown__container')
+boxForfilterCheckboxes.querySelectorAll('input:checked')
+boxForfilterCheckboxes.forEach(boxForcheckboxes => {
+  
+}); */
+
+/* let nweCheck = document.querySelectorAll('.filter-btn-dropdown__container')
+ */
 
 /* Отвечает за работу контейнера, перекрыващего карточку, когда нажата кнока с вопросом. 
 Необходимо в скрите, который будет подгружать карточки при перелистывании 
